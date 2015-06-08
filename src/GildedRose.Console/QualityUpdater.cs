063f94e3cf7a22fb892dd8a09db1952fcf073d3f
Marc Durham <v-mard@microsoft.com>
2015-06-08 10:17:07 -0700

    Remove unreferenced methods from QualityUpdater.

--- src/GildedRose.Console/QualityUpdater.cs
+++ src/GildedRose.Console/QualityUpdater.cs
@@ -32,53 +32,6 @@ namespace GildedRose.Console
             }
         }
 
-        private static void CalculateQuality(Item item)
-        {
-            if (DoesntAgeWell(item) && !IsLegendary(item))
-            {
-                AgeBadly(item);
-            }
-            else
-            {
-                AgeWell(item);
-            }
-
-            if (IsLastDayToSell(item) && IsEvent(item))
-            {
-                item.Quality = MIN_QUALITY;
-            };
-        }
-
-        private static bool IsLastDayToSell(Item item)
-        {
-            return item.SellIn == MIN_SELL_IN;
-        }
-
-        private static void AgeBadly(Item item)
-        {
-            if (QualityIsReducable(item))
-            {
-                if (item.SellIn > 0)
-                {
-                    item.Quality -= 1;
-                }
-                else
-                {
-                    item.Quality -= 2;
-                }
-            }
-        }
-
-        private static bool QualityIsReducable(Item item)
-        {
-            return StillHasQuality(item) && !IsLegendary(item);
-        }
-
-        private static bool DoesntAgeWell(Item item)
-        {
-            return !AgesWell(item) && !IsEvent(item);
-        }
-
         private static void CalculateSellIn(Item item)
         {
             if (!IsLegendary(item))
@@ -87,65 +40,9 @@ namespace GildedRose.Console
             }
         }
 
-        private static bool StillHasQuality(Item item)
-        {
-            return item.Quality > MIN_QUALITY;
-        }
-
-        private static bool QualityNotYetMax(Item item)
-        {
-            return item.Quality < MAX_QUALITY;
-        }
-
-        private static bool AgesWell(Item item)
-        {
-            return item.Name == "Aged Brie";
-        }
-
-        private static bool IsEvent(Item item)
-        {
-            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
-        }
-
         private static bool IsLegendary(Item item)
         {
             return item.Name == "Sulfuras, Hand of Ragnaros";
-        }
-
-        private static void AgeWell(Item item)
-        {
-            if (QualityNotYetMax(item))
-            {
-                item.Quality += 1;
-
-                AgeEventWell(item);
-            }
-        }
-
-        private static void AgeEventWell(Item item)
-        {
-            if (IsEvent(item))
-            {
-                if (EventIsSoon(item))
-                {
-                    item.Quality += 1;
-                }
-
-                if (EventIsVerySoon(item))
-                {
-                    item.Quality += 1;
-                }
-            }
-        }
-
-        private static bool EventIsVerySoon(Item item)
-        {
-            return item.SellIn < EVENT_VERY_SOON;
-        }
-
-        private static bool EventIsSoon(Item item)
-        {
-            return item.SellIn < EVENT_SOON;
         }
     }
 }
